ba48e4546fc83cabd3984e3f007b042e803ff027
DWL22 <candra.icy@gmail.com>
2025-08-12 16:17:48 +0800

    v8 done, lanjut ke perbandingan menggunakan vanilla js dan vue

--- counter-vue/src/App.vue
+++ counter-vue/src/App.vue
@@ -1,11 +1,57 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <h1>Counter</h1>
+    <span id="value">{{ count }}</span>
+    <div>
+      <button @click="count++" id="increment">+</button>
+      <button @click="count--" id="decrement">-</button>
+      <button id="reset">Reset</button>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  text-align: center;
+  background-color: #333333;
+  padding: 20px;
+  border-radius: 8px;
+}
+
+#value {
+  font-size: 50px;
+}
+button {
+  font-size: 16px;
+  margin: 0 5px;
+  background-color: #555555;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 4px;
+  color: #ffffff;
+}
+
+button:hover {
+  background-color: #777777;
+}
+button:active {
+  animation: pulse 0.5s linear;
+}
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
