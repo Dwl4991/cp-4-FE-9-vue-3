4eede0b1375adb1c8f044af7907f26e30d160644
DWL22 <candra.icy@gmail.com>
2025-10-16 11:04:19 +0800

    belajar vie 3 awal selesai gengs

---
+++ quiz-vue/src/components/QuizResult.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
+const { quizQuestionLength, numberOfCorrectAnswers } = defineProps([
+  "quizQuestionLength",
+  "numberOfCorrectAnswers",
+]);
+</script>
+<template>
+  <section class="results">
+    <p>Score:</p>
+    <h1>{{ numberOfCorrectAnswers }}/{{ quizQuestionLength }}</h1>
+    <RouterLink to="/">Go Back</RouterLink>
+  </section>
+</template>
+
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0;
+}
+p {
+  font-size: 24px;
+}
+h1 {
+  font-size: 80px;
+  margin: 20px 0;
+}
+</style>
--- quiz-vue/src/views/Quiz.vue
+++ quiz-vue/src/views/Quiz.vue
@@ -1,6 +1,8 @@
 <script setup>
 import QuizContent from "@/components/QuizContent.vue";
 import QuizHeader from "@/components/QuizHeader.vue";
+import QuizResult from "@/components/QuizResult.vue";
+
 import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 import quizes from "../data/quizes.json";
@@ -9,8 +11,9 @@ const route = useRoute();
 const quizId = parseInt(route.params.id);
 const quiz = quizes.find((q) => q.id === quizId);
 const numberOfCorrectAnswers = ref(0);
-
 const currentQuestionIndex = ref(0);
+const showResults = ref(false);
+
 const questionPage = computed(() => {
   return `${currentQuestionIndex.value + 1}/${quiz.questions.length}`;
 });
@@ -22,21 +25,26 @@ function onSelectOption(option) {
   if (option.correct) {
     numberOfCorrectAnswers.value++;
   }
+
+  if (currentQuestionIndex.value === quiz.questions.length - 1) {
+    showResults.value = true;
+    return;
+  }
   currentQuestionIndex.value++;
 }
 </script>
 <template>
   <QuizHeader :questionPage="questionPage" :barPercentage="barPercentage" />
   <QuizContent
+    v-if="!showResults"
     :question="quiz.questions[currentQuestionIndex]"
     @selectOption="onSelectOption"
   />
-  <button
-    @click="currentQuestionIndex++"
-    :disabled="currentQuestionIndex === quiz.questions.length - 1"
-  >
-    next
-  </button>
+  <QuizResult
+    v-else
+    :quizQuestionLength="quiz.questions.length"
+    :numberOfCorrectAnswers="numberOfCorrectAnswers"
+  />
 </template>
 
 <style scoped></style>
